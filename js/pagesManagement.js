07fb203892a9c9d1193948826a2e2aa7fa58f365
ChrisJeamme <chrisjahim@free.fr>
2018-10-17 12:27:59 +0200

    feat: basic architecture of the program (not fi nish)

---
+++ js/pagesManagement.js
@@ -0,0 +1,6 @@
+
+var pageInitialisation = function()
+{
+    document.querySelector('#encodingSection').classList.add('hide');
+    document.querySelector('#attackSection').classList.add('hide');
+}
