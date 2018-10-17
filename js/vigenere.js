6f32433ca11bb4e71e5b417ba7a2aed17238a211
Chris <chrisjahim@free.fr>
2018-10-17 23:22:12 +0200

    feat: begin of encoding (not finished)

--- js/vigenere.js
+++ js/vigenere.js
@@ -2,6 +2,30 @@
 var encoding = function(message,key)
 {
     // TODO
+    var repeatedKey = [], j=-1;
+    for(var i=0; i<message.length; i++)
+    {
+        j++;
+        if(j >= key.length)
+            j=0;
+        repeatedKey[i] = key[j];
+    }
+
+    console.log("message=\t"+message.split(''));
+    console.log("key=\t\t"+key.split(''));
+    console.log("repeatedKey=\t"+repeatedKey);
+
+    var result = [];
+
+    message.split('').forEach(
+    (messageChar,index)=>
+    {
+        console.log('M='+messageChar);
+        console.log('K='+repeatedKey[index]);
+        console.log('Resultat='+(String.fromCharCode(97+messageChar.charCodeAt(0)-repeatedKey[index].charCodeAt(0))))
+        
+    });
+
     return "encoding("+message+","+key+")";
 }
 var decoding = function(message,key)
