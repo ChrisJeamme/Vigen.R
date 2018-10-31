1d56a717d613fb910aac6980f285ca39c47fc4f7
dimitribruyere <dimitri.bruyere@outlook.fr>
2018-10-31 21:32:03 +0100

    feat: fill object with with 3 characters subsequences and their index

--- js/kasiski.js
+++ js/kasiski.js
@@ -1,3 +1,16 @@
 const findKeyLength = function(message) {
-    console.info(message);
+    let sequences = {};
+    let interestingSequences = [];
+    for (let i=0; i<message.length-2; i++){
+        let substr = message.substring(i, i+3);
+        if (sequences[substr]){ //On a déjà vu une séquence identique
+            interestingSequences.push(substr);
+        }
+        else {
+            sequences[substr] = []
+        }
+        sequences[substr].push(i);
+    }
+    console.log(sequences);
+    console.log(interestingSequences);
 }
