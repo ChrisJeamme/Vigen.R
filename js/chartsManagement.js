a8819b02c6f5f26a7e79bbef81d554a24f1b4735
Chris <chrisjahim@free.fr>
2018-11-15 05:11:35 +0100

    feat: saving charts in an array

--- js/chartsManagement.js
+++ js/chartsManagement.js
@@ -1,3 +1,4 @@
+let charts = [];
 
 const displayFrequencyChart = function(frequency, number)
 {
@@ -40,20 +41,22 @@ const displayFrequencyChart = function(frequency, number)
     <li id="chartButton`+number+`"><a onClick='displayChart(`+number+`)'>L`+(number+1)+`</a></li>
     `);
 
-    new Chart(
+    charts.push(new Chart(
         document.getElementById("chart"+number),
         {
             "type":"bar",
             "data":
             {
                 "labels":"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
-                "datasets":[{"label":"Occurences pour L"+(number+1),"data":displayableFrequencies,"fill":true,
-                "backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 159, 64, 0.2)","rgba(255, 205, 86, 0.2)","rgba(75, 192, 192, 0.2)","rgba(54, 162, 235, 0.2)","rgba(153, 102, 255, 0.2)","rgba(201, 203, 207, 0.2)"],
-                "borderColor":["rgb(255, 99, 132)","rgb(255, 159, 64)","rgb(255, 205, 86)","rgb(75, 192, 192)","rgb(54, 162, 235)","rgb(153, 102, 255)","rgb(201, 203, 207)"],
+                "datasets":[{"label":"Occurences pour L"+(number+1),
+                "data":displayableFrequencies,
+                "fill":true,
+                "backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)"],
+                "borderColor":["rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)"],
                 "borderWidth":1}]
             },
             "options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}
-        });
+        }));
 }
 
 const resetCharts = function()
