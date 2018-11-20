774f91fe2e8b40046e4f1e7afb2ae47a6cb7a986
Chris <chrisjahim@free.fr>
2018-11-20 20:47:09 +0100

    feat: add languages values in charts

--- js/chartsManagement.js
+++ js/chartsManagement.js
@@ -14,13 +14,15 @@ const createFrequencyChart = function(useless,number)
 const displayFrequencyChart = function(number)
 {
     let occurency = occurencies[number];
-
+    
     // Transformation en fréquences affichables en graphique
     let displayableOccurencies = [];
     let letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
     for(let i=0; i<26; i++)
         displayableOccurencies[i] = occurency[letters[i]];
-
+    
+    let numberOfLetterInText = displayableOccurencies.reduce((a,b)=>a+b,0);
+    
     // Remplissage du canva avec le graphique
     new Chart(
         document.querySelector("#chart"+number),
@@ -29,12 +31,33 @@ const displayFrequencyChart = function(number)
             "data":
             {
                 "labels":"ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(''),
-                "datasets":[{"label":"Occurences pour L"+(number+1),
-                "data":displayableOccurencies,
-                "fill":true,
-                "backgroundColor":["rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)","rgba(255, 99, 132, 0.2)"],
-                "borderColor":["rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)","rgb(255, 99, 132)"],
-                "borderWidth":1}]
+                "datasets":
+                [
+                    {
+                        "label":"Occurences pour L"+(number+1),
+                        "data":displayableOccurencies,
+                        "fill":true,
+                        "backgroundColor":Array(26).fill("rgba(255, 99, 132, 0.2)"),
+                        "borderColor":Array(26).fill("rgb(255, 99, 132)"),
+                        "borderWidth":1
+                    },
+                    {
+                        "label":"Occurences en français",
+                        "data":frenchFrequency.map(f=>f*numberOfLetterInText),
+                        "fill":true,
+                        "backgroundColor":Array(26).fill("rgba(116, 185, 255,1.0)"),
+                        "borderColor":Array(26).fill("rgba(9, 132, 227,1.0)"),
+                        "borderWidth":1
+                    },
+                    {
+                        "label":"Occurences en anglais",
+                        "data":englishFrequency.map(f=>f*numberOfLetterInText),
+                        "fill":true,
+                        "backgroundColor":Array(26).fill("rgba(0, 185, 255,1.0)"),
+                        "borderColor":Array(26).fill("rgba(0, 185, 255,0.8)"),
+                        "borderWidth":1
+                    }
+                ]
             },
             "options":{"scales":{"yAxes":[{"ticks":{"beginAtZero":true}}]}}
         });
